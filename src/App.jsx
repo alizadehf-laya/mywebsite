1738e5ede5e43d30a0d64f5665dbcf8849ef1706
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-10-30 11:18:57 +0330

    dashbord and img added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Hedear from "./components/Hedear"
+import Hedear from "./components/Header/Hedear"
+import styles from "./components/Header/Hedear.module.css"
 
+
+import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
 
 
   return (
     <>
-    <Hedear/>
-    <h1>hi</h1>
+ <Router>
+<div className="App">
+<Hedear />
+</div>
+</Router>
+  
     </>
   )
 }
