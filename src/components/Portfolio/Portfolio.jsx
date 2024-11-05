b29d4f867592c39bf7ce06ded6388c63ad80bc68
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-11-05 23:21:21 +0330

    still working

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,15 @@
 import styles from "./Portfolio.module.css"
 import Slidbar from "../Header/Slidebar";
+import CardPorto from "./CardPorto";
 const Portfolio=()=>{
     return(
         <>
        <div className={styles.main}>
             <div className={styles.left}><Slidbar height={"100vh"}/></div>
-            <div className={styles.right}></div>
+            <div className={styles.right}>
+                <div className="part1"><CardPorto/></div>
+                <div className="part2"></div>
+            </div>
 
         </div>
         </>
