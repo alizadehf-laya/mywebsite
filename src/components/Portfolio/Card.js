d8402c70225ac14bba7ba8715620ff53d75a0b59
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-11-06 13:03:46 +0330

    working on cards

---
+++ src/components/Portfolio/Card.js
@@ -0,0 +1,9 @@
+const Card=[
+    {
+        Title:"Summary",
+        Subtitle:"Web Developer",
+        Text:"  i m laya a web developer,i currently learned React which turned into my favorite language.i've started studying camputer science 3 years ago.i've codede alot of websites as a sample such as AMAZON."
+        
+    }
+
+]
--- src/components/Portfolio/CardPorto.jsx
+++ src/components/Portfolio/CardPorto.jsx
@@ -2,16 +2,13 @@ import Card from 'react-bootstrap/Card';
 
 function CardPorto() {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem',marginTop:"30px" , border:"none"}}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Title></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+        <Card.Text> </Card.Text>
+        {/* <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link> */}
       </Card.Body>
     </Card>
   );
