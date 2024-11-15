88a11239cd1bd465a0aea8fbe179a78a62f5135d
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-11-15 19:53:14 +0330

    working

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,12 +1,14 @@
 import Slidbar from "../Header/Slidebar";
 import styles from "./Profile.module.css";
 import Ptext1 from "./Ptext1";
+import Pnavbar from "./Pnavbar";
 const Profile=()=>{
     return(
 <div className={styles.mainPro}>
     <div className={styles.left}><Slidbar height="100vh"/></div>
     <div className={styles.right}>
         <Ptext1/>
+        <Pnavbar/>
     </div>
 </div>
     )
--- src/components/profile/Ptext1.jsx
+++ src/components/profile/Ptext1.jsx
@@ -4,11 +4,11 @@ function Ptext1() {
   return (
     <Card style={{border:"none" }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Portfolio</Card.Title>
+        <hr style={{color:"darkblue",borderWidth:"3px",color:"rgb(0, 119, 255)",width:"150px"}}/>
         {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+         This are some pages and projects that i've codede as sample:
         </Card.Text>
         {/* <Card.Link href="#">Card Link</Card.Link>
         <Card.Link href="#">Another Link</Card.Link> */}
